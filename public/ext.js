a836abbd01cdf83e94916e631ff1254f6dd9c2ef
Ankur Nigam <ankur.nigam198@gmail.com>
2019-06-15 14:00:53 +0530

    view vote

--- public/ext.js
+++ public/ext.js
@@ -1,41 +1,75 @@
 const createPoll = () => {
     let value = {
-        ques : document.querySelector('#ques').value,
-        options : [{
-            name : document.querySelector('#op1').value,
-            value : 0
-        }]
-    }
-    let data = JSON.stringify(value)
-    console.log(data)
-    sendToBackend(data)
-}
+        ques: document.querySelector("#ques").value,
+        options: []
+    };
 
-const sendToBackend = (data) => {
-    return fetch("/polls",{
-        method: 'POST',
+    const allOptions = document.querySelectorAll(".op");
+    allOptions.forEach(op => {
+        if (op.value.length > 0) {
+            value.options.push({ name: op.value, value: 0 });
+        }
+    });
+
+    let data = JSON.stringify(value);
+    console.log(data);
+    sendToBackend(data);
+};
+
+const sendToBackend = data => {
+    return fetch("/polls", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json"
         },
-        body : data
+        body: data
     })
-    .then(response => response.json())
-    .then(vari => {
+        .then(response => response.json())
+        .then(vari => {
             console.log(vari);
             saveId(vari);
+            window.location.href = "/view.html";
         });
-}
+};
 
-const saveId = (val) => {   
-    Cookies.set("id",val.id);
-}
+const saveId = val => {
+    Cookies.set("id", val.id);
+};
 
+let vote = "";
 const getPoll = () => {
-    fetch(`/polls/${Cookies.get('id')}`)
-    .then(data => data.json())
-    .then(data => {
-        console.log(data)
-        document.querySelector('#ques').innerHTML = data.ques;
-        document.querySelector('#options').innerHTML = data.options[0].name;
-    })
-}
+    fetch(`/polls/${Cookies.get("id")}`)
+        .then(data => data.json())
+        .then(data => {
+            console.log(data);
+            document.querySelector("#ques").innerHTML = data.ques;
+            vote = data;
+            // document.querySelector("#options").innerHTML = data.options[0].name;
+            let html = "";
+            data.options.forEach(op => {
+                html += `<div>${
+                    op.name
+                } <input type="radio" name="check" class="tick"> <h4>${op.value}</h4> </div>`;
+            });
+            document.querySelector("#options").innerHTML = html;
+        });
+};
+
+const sendVote = () => {
+    let selectedVote = '';
+    const allOptions = document.querySelectorAll(".tick");
+
+    for (let index = 0; index < allOptions.length; index++) {
+        const element = allOptions[index];
+        if (element.checked) {
+            selectedVote = vote.options[index].name;
+            element.checked=false
+            break;
+        }
+    }
+    console.log(selectedVote)
+
+    if(selectedVote.length>0){
+        fetch(`/polls/${vote.id}/${selectedVote}`).then(x=>window.location.href = window.location.href)
+    }
+};
